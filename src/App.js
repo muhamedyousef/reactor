6a21ace6d10050c166461fa495999ee1f43f50dd
muhamedyousef <mohhamedyousef99@gmail.com>
2020-11-11 21:06:27 +0200

    the first commit ever

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
 export default function App() {
+  let [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>{count}</h1>
+      <button onClick = {()=> setCount(count+1)}>increase the count</button>
     </div>
   );
 }
